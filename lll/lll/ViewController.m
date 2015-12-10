7916767b230e77c1cdb60ef8077bdd92e23763f6
jiangshouyuqaq <1264858678@qq.com>
2015-12-10 10:10:36 +0800

    jsy 10:10

--- lll/lll/ViewController.m
+++ lll/lll/ViewController.m
@@ -16,6 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.view.backgroundColor=[UIColor whiteColor];
+    self.navigationItem.title=@"测试";
     // Do any additional setup after loading the view, typically from a nib.
 }
 
